1ef5edb075cd1e6980fdc6389c55be72fe33d202
Vicente Javier Viera Guízar <plantedcoot706@gmail.com>
2024-11-12 17:16:49 -0600

    Netlify adapter added. - Configured enviromental variables schema.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-import icon from 'astro-icon';
-
-import svelte from '@astrojs/svelte';
+import { defineConfig, envField } from "astro/config";
+import icon from "astro-icon";
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), svelte()]
+  integrations: [tailwind(), icon(), svelte()],
+  output: "server",
+  adapter: netlify(),
+  experimental: {
+    env: {
+      schema: {
+        API_KEY: envField.string({context: "client", access: "public", default: "TEST"})
+      }
+    }
+  }
 });
