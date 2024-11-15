f88bf8c103bd28f4e47edc363858a8db2955541c
Vicente Javier Viera Guízar <plantedcoot706@gmail.com>
2024-11-15 15:35:56 -0600

    test

--- netlify/functions/createProcess.ts
+++ netlify/functions/createProcess.ts
@@ -1,16 +1,17 @@
 import type { Context } from "@netlify/functions";
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const uri = "mongodb+srv://vicentejvg:3tUFwiOVpHqcIYDl@maincluster.ylign.mongodb.net/?retryWrites=true&w=majority&appName=mainCluster"
-const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-});
 
 export default async (req: Request, context: Context) => {
+    const uri = "mongodb+srv://vicentejvg:3tUFwiOVpHqcIYDl@maincluster.ylign.mongodb.net/?retryWrites=true&w=majority&appName=mainCluster"
+    const client = new MongoClient(uri, {
+        serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+        }
+    });
+    
     await client.connect();
     const db = client.db("workin");
     const collection = db.collection("process");
